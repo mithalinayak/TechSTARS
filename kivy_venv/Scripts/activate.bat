941bb8879042e15ede311fb8fb300b07e3a0bc68
Mithali Nayak <101163706+mithalinayak@users.noreply.github.com>
2024-03-18 15:18:56 +0530

    simple interface

--- kivy_venv/Scripts/activate.bat
+++ kivy_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\akansha\OneDrive\Desktop\TechSTARS\kivy_venv
+set VIRTUAL_ENV=C:\Users\mitha\OneDrive\Desktop\Detection app\TechSTARS\kivy_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
